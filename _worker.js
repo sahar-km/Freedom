b081c14dcea4645982f00c7048859922faee5bc4
Roseberry <165325276+sahar-km@users.noreply.github.com>
2024-04-12 19:22:24 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,15 +1,23 @@
 // @ts-nocheck
+// <!--GAMFC-->version base on commit 43fad05dcdae3b723c53c226f8181fc5bd47223e, time is 2023-06-22 15:20:02 UTC<!--GAMFC-END-->.
 // @ts-ignore
+// Many thanks to github.com/bia-pain-bache
 
 import { connect } from 'cloudflare:sockets';
 
+// How to generate your own UUID:
+// https://www.uuidgenerator.net/
 let userID = '7fff5c98-b6ae-4081-92e0-3f510e256741';
+
+
 
 const proxyIPs= ['workers.cloudflare.cyou'];// OR USE const proxyIPs = ['cdn.xn--b6gac.eu.org', 'cdn-all.xn--b6gac.eu.org', 'edgetunnel.anycast.eu.org'];
 
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
 
 let dohURL = 'https://cloudflare-dns.com/dns-query';
+
+let panelVersion = '2.3.2';
 
 if (!isValidUUID(userID)) {
     throw new Error('uuid is not valid');
@@ -843,9 +851,7 @@ const getNormalConfigs = async (env, hostName, client) => {
             randomUpperCase(hostName)
         }&sni=${
             randomUpperCase(hostName)
-        }&fp=randomized&alpn=http/1.1&path=${
-            encodeURIComponent(`/${getRandomPath(16)}?ed=2048`)
-        }#${encodeURIComponent(remark)}\n`;
+        }&fp=randomized&alpn=http/1.1&path=/${getRandomPath(16)}#${encodeURIComponent(remark)}\n`;
     });
 
     const subscription = client === 'singbox' ? btoa(vlessWsTls) : btoa(vlessWsTls.replaceAll('http/1.1', 'http/1.1'));
@@ -1000,7 +1006,7 @@ const getFragmentConfigs = async (env, hostName, client) => {
 
         let fragConfig = structuredClone(xrayConfigTemp);
         let outbound = structuredClone(xrayOutboundTemp);
-        let remark = `💎  ${addr}`;
+        let remark = `💎 Frag - ${addr}`;
         delete outbound.mux;
         delete outbound.streamSettings.grpcSettings;
         delete outbound.streamSettings.realitySettings;
@@ -1010,7 +1016,7 @@ const getFragmentConfigs = async (env, hostName, client) => {
         outbound.settings.vnext[0].users[0].id = userID;
         outbound.streamSettings.tlsSettings.serverName = randomUpperCase(hostName);
         outbound.streamSettings.wsSettings.headers.Host = randomUpperCase(hostName);
-        outbound.streamSettings.wsSettings.path = `/${getRandomPath(16)}?ed=2048`;
+        outbound.streamSettings.wsSettings.path = `/${getRandomPath(16)}`;
         
         fragConfig.remarks = remark.length <= 30 ? remark : `${remark.slice(0,29)}...`;;
         fragConfig.dns.servers[0] = remoteDNS;
@@ -1125,6 +1131,7 @@ const getSingboxConfig = async (env, hostName) => {
         let outbound = structuredClone(singboxOutboundTemp);
         outbound.server = addr;
         outbound.tag = addr;
+	outbound.uuid = userID;
         outbound.tls.server_name = randomUpperCase(hostName);
         outbound.transport.headers.Host = randomUpperCase(hostName);
         outbound.transport.path += getRandomPath(16);
@@ -1503,7 +1510,7 @@ const renderHomePage = async (env, hostName, fragConfigs) => {
 	</head>
 	
 	<body>
-		<h1 style="text-align: center; color: #2980b9">🎃 BPB Panel <span style="font-size: smaller;">v2.3.1</span> </h1>
+		<h1 style="text-align: center; color: #2980b9">🎃 BPB Panel <span style="font-size: smaller;">v${panelVersion}</span> </h1>
 		<div class="form-container">
             <h2>FRAGMENT SETTINGS ⚙️</h2>
 			<form id="configForm">
@@ -1792,7 +1799,7 @@ const renderHomePage = async (env, hostName, fragConfigs) => {
             const intervalMax = getValue('fragmentIntervalMax');
             const cleanIP = document.getElementById('cleanIPs');
             const cleanIPs = cleanIP.value?.split(',');
-            const chainProxy = document.getElementById('outProxy').value;                    
+            const chainProxy = document.getElementById('outProxy').value?.trim();                    
             const formData = new FormData(configForm);
             const isVless = /vless:\\/\\/[^\s@]+@[^\\s:]+:[^\\s]+/.test(chainProxy);
             const hasSecurity = /security=/.test(chainProxy);
@@ -2005,7 +2012,7 @@ const renderLoginPage = async () => {
     </head>
     <body>
         <div class="container">
-            <h1>🎱 BPB Panel <span style="font-size: smaller;">v2.3.1</span>  </h1>
+            <h1>🎱 BPB Panel <span style="font-size: smaller;">${panelVersion}</span>  </h1>
             <div class="form-container">
                 <h2>User Login</h2>
                 <form id="loginForm">
@@ -2312,13 +2319,8 @@ const singboxConfigTemp = {
             {
                 outbound: "direct",
                 server: "dns-direct"
-            },
-            {
-                outbound: "any",
-                server: "dns-remote"
             }
         ],
-        strategy: "ipv4_only",
         independent_cache: true
     },
     inbounds: [
@@ -2355,11 +2357,11 @@ const singboxConfigTemp = {
         {
             type: "selector",
             tag: "proxy",
-            outbounds: ["URL-TEST"]
+            outbounds: ["Best-Ping"]
         },
         {
             type: "urltest",
-            tag: "URL-TEST",
+            tag: "Best-Ping",
             outbounds: [],
             url: "https://www.gstatic.com/generate_204",
             interval: "3m",
@@ -2497,7 +2499,7 @@ const singboxOutboundTemp = {
     type: "vless",
     server: "",
     server_port: 443,
-    uuid: userID,
+    uuid: "",
     domain_strategy: "prefer_ipv6",
     packet_encoding: "",
     tls: {
@@ -2555,7 +2557,7 @@ const errorPage = `
 
     <body>
         <div id="error-container">
-            <h1>BPB <span style="font-size: smaller;">v2.3.1</span> 🎃</h1>
+            <h1>BPB <span style="font-size: smaller;">${panelVersion}</span> 🎃</h1>
             <div id="error-message">
                 <h2>KV Dataset is not properly set! Please refer to <a href="https://github.com/bia-pain-bache/BPB-Worker-Panel/blob/main/README.md">documents</a></h2>
             </div>
